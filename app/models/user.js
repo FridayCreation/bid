064051f4040b2bd63c1b14d7748472b3802c4bb2
ku4ncheang <ku4ncheang@gmail.com>
2013-12-08 18:07:01 +0800

    add step 2

--- app/models/user.js
+++ app/models/user.js
@@ -63,11 +63,11 @@ var validatePresenceOf = function (value) {
 
 // the below 4 validations only apply if you are signing up traditionally
 
-UserSchema.path('name').validate(function (name) {
-  // if you are authenticating by any of the oauth strategies, don't validate
-  if (authTypes.indexOf(this.provider) !== -1) return true
-  return name.length
-}, 'Name cannot be blank')
+// UserSchema.path('name').validate(function (name) {
+//   // if you are authenticating by any of the oauth strategies, don't validate
+//   if (authTypes.indexOf(this.provider) !== -1) return true
+//   return name.length
+// }, 'Name cannot be blank')
 
 UserSchema.path('email').validate(function (email) {
   // if you are authenticating by any of the oauth strategies, don't validate
@@ -89,11 +89,11 @@ UserSchema.path('email').validate(function (email, fn) {
   } else fn(true)
 }, 'Email already exists')
 
-UserSchema.path('username').validate(function (username) {
-  // if you are authenticating by any of the oauth strategies, don't validate
-  if (authTypes.indexOf(this.provider) !== -1) return true
-  return username.length
-}, 'Username cannot be blank')
+// UserSchema.path('username').validate(function (username) {
+//   // if you are authenticating by any of the oauth strategies, don't validate
+//   if (authTypes.indexOf(this.provider) !== -1) return true
+//   return username.length
+// }, 'Username cannot be blank')
 
 UserSchema.path('hashed_password').validate(function (hashed_password) {
   // if you are authenticating by any of the oauth strategies, don't validate
--- public/js/app.js
+++ public/js/app.js
@@ -15,8 +15,87 @@ $(document).ready(function () {
 });
 
     
-angular.module('Stallket.Signup', ['ui']);
+angular.module('Stallket.Signup', ['ui', 'restangular', 'ngRoute']).
+  config(function($routeProvider, RestangularProvider) {
+    // $routeProvider.
+    //   when('/', {
+    //     controller:ListCtrl, 
+    //     templateUrl:'list.html'
+    //   }).
+    //   when('/edit/:projectId', {
+    //     controller:EditCtrl, 
+    //     templateUrl:'detail.html',
+    //     resolve: {
+    //       project: function(Restangular, $route){
+    //         return Restangular.one('projects', $route.current.params.projectId).get();
+    //       }
+    //     }
+    //   }).
+    //   when('/new', {controller:CreateCtrl, templateUrl:'detail.html'}).
+    //   otherwise({redirectTo:'/'});
+      
+      RestangularProvider.setBaseUrl('http://localhost:3000');
 
-function SignupCtrl($scope) {
- 
+      RestangularProvider.setResponseExtractor(function(response, operation) {
+        return response;
+      });
+
+      // RestangularProvider.setDefaultRequestParams({ apiKey: '4f847ad3e4b08a2eed5f3b54' })
+      // RestangularProvider.setRestangularFields({
+      //   id: '_id.$oid'
+      // });
+      
+      // RestangularProvider.setRequestInterceptor(function(elem, operation, what) {
+      //   if (operation === 'put') {
+      //     elem._id = undefined;
+      //     return elem;
+      //   }
+      //   return elem;
+      // })
+  });
+
+
+function SignupCtrl($scope, Restangular) {
+  var baseUsers = Restangular.all('users');
+  $scope.alerts = [];
+  $scope.step = 1;
+
+  $scope.create = function() {
+    $scope.alerts = [];
+
+    //sign up the form when it is valid
+    if ($scope.form.$valid) {
+
+      //post to api and create new user
+      baseUsers.post(angular.toJson($scope.user())).
+        then(function(result) {
+          if (result.errors === undefined) {
+            $scope.step = 2;
+          } else if (result.errors.email !== undefined){
+             $scope.pushAlert(result.errors.email.message);
+          }
+        }, function() {
+          console.log('There was an error saving');
+        });
+
+    } else {
+      $scope.pushAlert('請確定已塡寫所有資料');
+      //show error message
+    }
+  }
+
+  $scope.user = function() {
+
+    //convert form to json
+    var newUser = {
+      email: $scope.email,
+      hashed_password: $scope.password
+    };
+
+    return newUser;
+  }
+
+  $scope.pushAlert = function(message) {
+    $scope.alerts.push({msg: message});
+  }
 }
